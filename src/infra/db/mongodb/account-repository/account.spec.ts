45a8403ae943741a5cb410dac8bc5dbbc3784248
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-01 08:01:59 -0300

    test: update accessToken

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -56,4 +56,21 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeNull()
   })
+
+  // test('Should update the account accessToken on updateAccessToken success', async () => {
+  //   const sut = makeSut()
+  //   const result = await accountCollection.insertOne({
+  //     name: 'any_name',
+  //     email: 'any_email@mail.com',
+  //     password: 'any_password'
+  //   })
+  //   const { insertedId: id } = result
+  //   const prevAccount = await accountCollection.findOne({ _id: id })
+  //   if (prevAccount?._id) {
+  //     await sut.updateAccessToken(prevAccount?._id.toString(), 'any_token')
+  //     const account = await accountCollection.findOne({ _id: prevAccount?._id })
+  //     expect(account).toBeTruthy()
+  //     expect(account).toBe('any_token')
+  //   }
+  // })
 })
