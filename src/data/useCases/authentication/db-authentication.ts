48a45dbec424a147fd92ff3e77226a02e613b98b
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-12-01 07:32:46 -0300

    refactor: change load to loadByEmail in LoadAccountByEmailRepository

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -23,7 +23,7 @@ export class DbAuthentication implements Authentication {
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const { email, password } = authentication
-    const account = await this.loadAccountByEmailRepository.load(email)
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (account) {
       const isValid = await this.hashComparer.compare(password, account?.password)
       if (isValid) {
